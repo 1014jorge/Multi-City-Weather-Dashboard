0c9f85f3887b4d48346de95c70fe69d5794e46d5
Jorge Gonzalez <jorge.1014@gmail.com>
2023-08-09 14:04:04 -0700

    updated html,javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,17 @@
-var weatherAPI = aed97915b0e1e75acefce00314eef350
+const weatherApi = "1354cc726d8de2a117f45eeb347ece87";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+
+async function checkWeather() {
+    const response = await fetch(apiURL + '&appid=${weatherApi}');
+    var data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " mph";
+}
+
+checkWeather();
+
+
